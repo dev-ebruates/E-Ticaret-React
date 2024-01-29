7cac6fa217bf5a1a9eb380f48147f12716f1b861
ebru <devatesebru@gmail.com>
2024-01-29 17:41:23 +0300

    HomePage componenti oluşturuldu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,13 @@
-import Category from './components/Categories/Category'
-import Footer from './components/Layout/Footer/Footer'
-import Header from './components/Layout/Header/Header'
-import Policy from './components/Layout/Policy/Policy'
-import Sliders from './components/Slider/Sliders'
-import Products from './components/Products/Products'
+import React from 'react'
 import './App.css'
-import Campaigns from './components/Campaigns/Campaigns'
-import Blogs from './components/Blogs/Blogs'
-import Brands from './components/Brands/Brands'
-import CampaignSingle from './components/CampaignSingle/CampaignSingle'
+import { HomePage } from './pages/HomePage'
 
 function App() {
  
   return (
-    <>
-    <Header/>
-     <Sliders/>
-     <Category/>
-     <Products/>
-     <Campaigns/>
-     <Products/>
-     <Blogs/>
-     <Brands/>
-     <CampaignSingle/>
-     <Policy/>
-     <Footer/>
-    </>
+    <React.Fragment>
+    <HomePage/>
+    </React.Fragment>
   )
 }
 
