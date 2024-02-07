d2976d1d84e977757f4eef81e957f7f50006c633
ebru <devatesebru@gmail.com>
2024-02-07 15:34:30 +0300

    LocalStorage'e sepetteki ürünleri kayıt edildi.

--- frontend/src/context/CardProvider.jsx
+++ frontend/src/context/CardProvider.jsx
@@ -1,14 +1,23 @@
-import { createContext, useState } from "react";
-import PropTypes from  "prop-types"
+import { createContext, useEffect, useState } from "react";
+import PropTypes from "prop-types";
 
 export const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
-  const[cartItems, setCardItems] = useState([])
+  const [cartItems, setCardItems] = useState(
+    localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }, [cartItems]);
+
   // console.log(cartItems);
   const addToCart = (product) => {
     // setCardItems([...cartItems, product]); 1.yöntem
-    setCardItems((prevItems) => [...prevItems, product]) //öncekileri al ona product ı ekle
+    setCardItems((prevItems) => [...prevItems, product]); //öncekileri al ona product ı ekle
   };
   return (
     <CartContext.Provider
@@ -23,6 +32,6 @@ const CartProvider = ({ children }) => {
 };
 export default CartProvider;
 
-CartProvider.propTypes={
-  children:PropTypes.node,
+CartProvider.propTypes = {
+  children: PropTypes.node,
 };
