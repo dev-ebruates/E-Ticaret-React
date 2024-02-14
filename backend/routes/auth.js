6b2a19fa29ef63f5f535acaa8affa387ba885f3e
ebru <devatesebru@gmail.com>
2024-02-14 12:55:54 +0300

    Güvenli password için kütüphane yüklendi("bcryptjs") ve uygulandı . Email adresi bir kere kullanilirlik uygulandı.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,18 +1,30 @@
 const express = require("express");
+const bcryptjs = require("bcryptjs");
 const router = express.Router();
 const User = require("../models/User.js");
 
 //kullanıcı oluşturma(create- user)
 router.post("/register", async (req, res) => {
   try {
-    const {username, email, password} =req.body;
+    const { username, email, password } = req.body;
+
+    //email adresi bir kere kullanılabilir
+    //return yaptığımız için aşağıya girmeden devam edecek
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      res.status(400).json({ error: "Email adress is already registed." });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
     const newUser = await new User({
-      username, email, password
-    })
-    console.log(newUser)
-    //veritabanına kayıt ettik 
+      username,
+      email,
+      password: hashedPassword,
+    });
+    console.log(newUser);
+    //veritabanına kayıt ettik
     await newUser.save();
-    res.status(201).json(newUser)
+    res.status(201).json(newUser);
   } catch (error) {
     res.status(500).json({ error: "Server Error" });
   }
