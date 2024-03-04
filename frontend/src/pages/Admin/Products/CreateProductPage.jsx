21d21a44451c412c7c30c9a061aa9824d896494e
ebru <devatesebru@gmail.com>
2024-03-04 22:53:43 +0300

    yeni product oluşturma

--- frontend/src/pages/Admin/Products/CreateProductPage.jsx
+++ frontend/src/pages/Admin/Products/CreateProductPage.jsx
@@ -1,22 +1,62 @@
-import { Form, Input, InputNumber, Select, Spin, message } from "antd";
-import { useState } from "react";
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import { Button, Form, Input, InputNumber, Select, Spin, message } from "antd";
+import { useEffect, useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const apiUrl = import.meta.env.VITE_API_BASE_URL;
 
 const CreateProductPage = () => {
   const [loading, setLoading] = useState(false);
+  const [categories, setCategories] = useState([]);
+
   const [form] = Form.useForm();
 
+  useEffect(() => {
+    const fetchCategory = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fetch(`${apiUrl}/api/categories`);
+        if (response.ok) {
+          const data = await response.json();
+          setCategories(data);
+        } else {
+          message.error("Veri Getirme Başarısız.");
+        }
+      } catch (error) {
+        console.log("Veri Hatası:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCategory();
+  }, [apiUrl]);
+  console.log(categories);
+
   const onFinish = async (values) => {
+    console.log(values);
+    const imgLinks = values.img.split("\n").map((link) => link.trim());
+    const colors = values.colors.split("\n").map((link) => link.trim());
+    const sizes = values.sizes.split("\n").map((link) => link.trim());
+
+    console.log(imgLinks);
+
     setLoading(true);
     try {
       console.log(values);
       const response = await fetch(`${apiUrl}/api/products/`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(values),
+        body: JSON.stringify({
+          ...values,
+          price: {
+            current: values.current,
+            discount: values.discount,
+          },
+          colors,
+          sizes,
+          img:imgLinks,
+        }),
       });
       if (response.ok) {
         message.success("Ürün başarıyla oluşturuldu.");
@@ -46,6 +86,24 @@ const CreateProductPage = () => {
           ]}
         >
           <Input />
+        </Form.Item>
+        <Form.Item
+          label="Ürün Kategorisi"
+          name="category"
+          rules={[
+            {
+              required: true,
+              message: "Lütfen 1 kategori seçin!",
+            },
+          ]}
+        >
+          <Select>
+            {categories?.map((category) => (
+              <Select.Option value={category._id} key={category._id}>
+                {category.name}
+              </Select.Option>
+            ))}
+          </Select>
         </Form.Item>
         <Form.Item
           label="Fiyat"
@@ -81,12 +139,14 @@ const CreateProductPage = () => {
             },
           ]}
         >
-        {/* react ouill uygulandı */}
-          <ReactQuill theme="snow"  style={{
-            backgroundColor:"white"
-          }}/>
+          {/* react ouill uygulandı */}
+          <ReactQuill
+            theme="snow"
+            style={{
+              backgroundColor: "white",
+            }}
+          />
         </Form.Item>
-
 
         <Form.Item
           label="Ürün görselleri (link)"
@@ -133,25 +193,10 @@ const CreateProductPage = () => {
             autoSize={{ minRows: 4 }}
           />
         </Form.Item>
-        <Form.Item
-          label="Ürün Kategorisi"
-          name="category"
-          rules={[
-            {
-              required: true,
-              message: "Lütfen 1 kategori seçin!",
-            },
-          ]}
-        >
-          <Select>
-            <Select.Option value="SmartPhone" key={"SmartPhone"}>SmartPhone</Select.Option>
-          </Select>
-        </Form.Item>
 
-        {/* 
         <Button type="primary" htmlType="submit">
           Oluştur
-        </Button> */}
+        </Button>
       </Form>
     </Spin>
   );
