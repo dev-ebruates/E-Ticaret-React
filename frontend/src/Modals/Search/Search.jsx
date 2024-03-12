ba71cf3c7076f567b68c4e2432625ea676a4427a
ebru <devatesebru@gmail.com>
2024-03-12 12:08:15 +0300

    Product Search Front-End Bağlantısı

--- frontend/src/Modals/Search/Search.jsx
+++ frontend/src/Modals/Search/Search.jsx
@@ -1,6 +1,30 @@
+import { message } from "antd";
 import "./Search.css";
 import PropTypes from "prop-types";
 const Search = ({ isSearchShow, setIsSearchShow }) => {
+  const apiUrl = import.meta.env.VITE_API_BASE_URL;
+
+  const handleSearch = async(e)=>{
+    e.preventDefault();
+    const productName = e.target[0].value;
+
+    try {
+      const res = await fetch(
+        `${apiUrl}/api/products/search/${productName}`
+      );
+      if(!res.ok){
+        message.warning("Ürün getirme hatası...")
+        return;
+      }
+      const data = await res.json();
+
+      console.log(data);
+    } catch (error) {
+      console.log(error)
+      
+    }
+
+  }
   return (
     <div className={`modal-search ${isSearchShow ? "show" : ""}`}>
       <div className="modal-wrapper">
@@ -8,7 +32,7 @@ const Search = ({ isSearchShow, setIsSearchShow }) => {
         <p className="modal-text">
           Start typing to see products you are looking for.
         </p>
-        <form className="search-form">
+        <form className="search-form" onSubmit={handleSearch}>
           <input type="text" placeholder="Search a product" />
           <button>
             <i className="bi bi-search"></i>
